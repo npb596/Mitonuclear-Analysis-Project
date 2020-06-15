fae02158abbecb682a3cf9ec1cc97f0778496063
Nick Bailey <npb596@gmail.com>
2020-06-15 15:45:59 -0500

    Editing concatenate_exons.sh so that it can work with any number of individuals

--- Gene_processing/concatenate_exons.sh
+++ Gene_processing/concatenate_exons.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Define arrays based on Macaque individual names and the names of N-mt genes.
+# Define arrays based on Macaque individual names and the names of genes. Also create a variable for the double the number of individuals (to get the diploid number of genotypes)
 
 indv_file=$1
 gene_file=$2
 
 monkey_array=(`cat ${indv_file}`)
 gene_array=(`awk '{print $4}' ${gene_file}`)
+
+indv_number=`wc -l ${indv_file} | awk '{print $1}'`
+indv_number_doubled=`expr ${indv_number} \* 2`
 
 # Start loops for every gene name and for every fasta file in the folder (ordered numerically), which are named after the genes
 
@@ -22,8 +25,8 @@ exon_array=(`ls *${s}.fa | sort -n`)
 # So all this math gives us the amount of sequence lines per exon
 
 	file_count=`wc -l $filename | awk '{print $1}'`
-	minus_twenty=`expr $file_count - 26`
-	final_number=`expr $minus_twenty / 26`
+	minus_twenty=`expr $file_count - ${indv_number_doubled}`
+	final_number=`expr $minus_twenty / ${indv_number_doubled}`
 
 # Start a loop for each macaque individual name and create files with all exons of a particular gene for each individual macaque
 
