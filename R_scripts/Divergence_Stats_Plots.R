23a067d126a472e5f332c7193047874e42a4aabf
Nick Bailey <sambailey@Sams-MacBook-Pro.local>
2020-08-03 23:12:23 -0500

    Adding updated versions of Divergence and Introgression scripts with comments

--- R_scripts/Divergence_Stats_Plots.R
+++ R_scripts/Divergence_Stats_Plots.R
@@ -1,58 +1,61 @@
+# Load modules for plots
 library(ggplot2)
 library(ggpubr)
 library(plyr)
-library(gridExtra)
 
-setwd("~/Desktop/Macaque_files/mitonuclear_project/pop_gen_analysis")
+# Set working directory to Divergence Data
+setwd("~/Desktop/Macaque_files/mitonuclear_project/pop_gen_analysis/Divergence_Data")
 
-ALL.dxy=read.table("Divergence_Data/Dxy_Sin_Arc_CDS.txt",header=F,stringsAsFactors = T)
-ETS.dxy=read.table("Divergence_Data/ETS_Sin_Arc_Dxy.txt",header=F,stringsAsFactors = T)
-MRP.dxy=read.table("Divergence_Data/ribosomal_Sin_Arc_Dxy.txt",header=F,stringsAsFactors = T)
-MTS.dxy=read.table("Divergence_Data/synthetase_Sin_Arc_Dxy.txt",header=F,stringsAsFactors = T)
-MISC.dxy=read.table("Divergence_Data/miscellaneous_Sin_Arc_Dxy.txt",header=F,stringsAsFactors = T)
-colnames(ALL.dxy)<-c("ALL.dxy","ALL.DNAType")
-colnames(ETS.dxy)<-c("ETS.dxy","Gene","ETS.DNAType")
-colnames(MRP.dxy)<-c("MRP.dxy","Gene","MRP.DNAType")
-colnames(MTS.dxy)<-c("MTS.dxy","Gene","MTS.DNAType")
-colnames(MISC.dxy)<-c("MISC.dxy","Gene","MISC.DNAType")
+# Define tables from divergence data files
+ALL.dxy=read.table("ALL_Combined_Dxy.txt",header=T,stringsAsFactors = T)
+ETS.dxy=read.table("ETS_Combined_Dxy.txt",header=T,stringsAsFactors = T)
+MRP.dxy=read.table("ribosomal_Combined_Dxy.txt",header=T,stringsAsFactors = T)
+MTS.dxy=read.table("synthetase_Combined_Dxy.txt",header=T,stringsAsFactors = T)
+MISC.dxy=read.table("miscellaneous_Combined_Dxy.txt",header=T,stringsAsFactors = T)
 
-mod_ALL.dxy <- ddply(ALL.dxy, "ALL.DNAType", summarise, grp.mean=mean(ALL.dxy))
-mod_ETS.dxy <- ddply(ETS.dxy, "ETS.DNAType", summarise, grp.mean=mean(ETS.dxy))
-mod_MRP.dxy <- ddply(MRP.dxy, "MRP.DNAType", summarise, grp.mean=mean(MRP.dxy))
-mod_MTS.dxy <- ddply(MTS.dxy, "MTS.DNAType", summarise, grp.mean=mean(MTS.dxy))
-mod_MISC.dxy <- ddply(MISC.dxy, "MISC.DNAType", summarise, grp.mean=mean(MISC.dxy))
-
-ALL_plot <- ggplot(ALL.dxy, aes(x=ALL.dxy, color=ALL.DNAType)) +
-  geom_density() + labs(x="Dxy", y = "Density") + theme(legend.position = "none") +
+# Plot all data with titles, x and y labels, colors filling violin plots
+ALL_plot <- ggplot(ALL.dxy, aes(x=ALL.Comparison, y=ALL.dxy, fill = ALL.DNAType)) +
+  labs(title="All Functional Categories", x ="Species", y="Dxy") +
+  geom_violin(trim=FALSE) + theme(legend.position = "none") +
   theme(legend.title=element_text(size=50)) + theme(legend.text=element_text(size=45)) +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
-  geom_vline(data=mod_ALL.dxy, aes(xintercept=grp.mean, color=ALL.DNAType), linetype="dashed") + 
-  theme(text = element_text(size=29))
-ETS_plot <- ggplot(ETS.dxy, aes(x=ETS.dxy, color=ETS.DNAType)) +
-  geom_density() + labs(x="Dxy", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
-  geom_vline(data=mod_ETS.dxy, aes(xintercept=grp.mean, color=ETS.DNAType), linetype="dashed") +
-  theme(text = element_text(size=19))
-MRP_plot <- ggplot(MRP.dxy, aes(x=MRP.dxy, color=MRP.DNAType)) +
-  geom_density() + labs(x="Dxy", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
-  geom_vline(data=mod_MRP.dxy, aes(xintercept=grp.mean, color=MRP.DNAType), linetype="dashed") +
-  theme(text = element_text(size=19))
-MTS_plot <- ggplot(MTS.dxy, aes(x=MTS.dxy, color=MTS.DNAType)) +
-  geom_density() + labs(x="Dxy", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
-  geom_vline(data=mod_MTS.dxy, aes(xintercept=grp.mean, color=MTS.DNAType), linetype="dashed") + 
-  theme(text = element_text(size=19))
-MISC_plot <- ggplot(MISC.dxy, aes(x=MISC.dxy, color=MISC.DNAType)) +
-  geom_density() + labs(x="Dxy", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
-  geom_vline(data=mod_MISC.dxy, aes(xintercept=grp.mean, color=MISC.DNAType), linetype="dashed") + 
-  theme(text = element_text(size=19))
+  scale_fill_manual(name = "Gene Type", values=c("#af8dc3","#7fbf7b")) + 
+  theme(text = element_text(size=29)) + geom_text(x=2.1, y=0.65, label="N = 101, P = 0.009", size=10) + 
+  geom_boxplot(width = 0.2, position = position_dodge(0.9))
+ETS_plot <- ggplot(ETS.dxy, aes(x=ETS.Comparison, y=ETS.dxy, fill = ETS.DNAType)) +
+  labs(title="Electron Transport System", x ="Species", y="Dxy") + 
+  geom_violin(trim=FALSE) + theme(legend.position = "none") +
+  scale_fill_manual(values=c("#af8dc3","#7fbf7b")) +
+  theme(text = element_text(size=19)) + geom_text(x=2.1, y=0.65, label="N = 49, P = 0.047", size=5) +
+  geom_boxplot(width = 0.2, position = position_dodge(0.9))
+MRP_plot <- ggplot(MRP.dxy, aes(x=MRP.Comparison, y=MRP.dxy, fill = MRP.DNAType)) +
+  labs(title="Mito Ribosomal Protein", x ="Species", y="Dxy") + 
+  geom_violin(trim=FALSE) + theme(legend.position = "none") +
+  scale_fill_manual(name = "Gene Type", values=c("#af8dc3","#7fbf7b")) +
+  theme(text = element_text(size=19)) + geom_text(x=2.1, y=0.6, label="N = 29, P = 0.036", size=5) +
+  geom_boxplot(width = 0.15, position = position_dodge(0.9))
+MTS_plot <- ggplot(MTS.dxy, aes(x=MTS.Comparison, y=MTS.dxy, fill = MTS.DNAType)) +
+  labs(title="Mito tRNA-synthetase", x ="Species", y="Dxy") + 
+  geom_violin(trim=FALSE) + theme(legend.position = "none") +
+  scale_fill_manual(name = "Gene Type", values=c("#af8dc3","#7fbf7b")) +
+  theme(text = element_text(size=19)) + geom_text(x=2.1, y=0.4, label="N = 10, P = 0.804", size=5) +
+  geom_boxplot(width = 0.1, position = position_dodge(0.9))
+MISC_plot <- ggplot(MISC.dxy, aes(x=MISC.Comparison, y=MISC.dxy, fill = MISC.DNAType)) +
+  labs(title="Transcription/translation", x ="Species", y="Dxy") + 
+  geom_violin(trim=FALSE) + theme(legend.position = "none") +
+  scale_fill_manual(name = "Gene Type", values=c("#af8dc3","#7fbf7b")) +
+  theme(text = element_text(size=19)) + geom_text(x=2.1, y=0.45, label="N = 13, P = 0.051", size=5) +
+  geom_boxplot(width = 0.2, position = position_dodge(0.9))
 
+# Create legend using info from ALL_plot above
 ALL_legend <- get_legend(ALL_plot, position = "bottom")
 
+# Combine all plots defined above and add labels
 ggarrange(ALL_plot,
           ggarrange(ETS_plot,MRP_plot,MTS_plot,MISC_plot,
-                    labels = c("B", "C", "D","E"), label.x = 0.12, font.label = list(size = 27),
-                    ncol = 2, nrow = 2), labels="A", label.x = 0.12, font.label = list(size = 37), 
+                    labels = c("B", "C", "D","E"), label.x = 0.17, label.y = 0.91, font.label = list(size = 27),
+                    ncol = 2, nrow = 2), labels="A", label.x = 0.12, label.y = 0.93, font.label = list(size = 37), 
           legend.grob = ALL_legend)
+
+# Input file must be redefined to one including only Arc-Sin or Arc-Fas data
+# Then change variables and alternative accordingly, then conduct wilcoxon rank-sum test
+wilcox.test(ALL.dxy ~ ALL.DNAType, data=ALL.dxy, alternative="greater")
--- R_scripts/Introgression_Stats_Plots.R
+++ R_scripts/Introgression_Stats_Plots.R
@@ -1,63 +1,68 @@
+# Load modules for plots and summary statistics
 library(ggplot2)
 library(ggpubr)
 library(plyr)
 library(gridExtra)
 
-setwd("~/Desktop/Macaque_files/mitonuclear_project/pop_gen_analysis")
+# Set working directory to Introgression Data
+setwd("~/Desktop/Macaque_files/mitonuclear_project/pop_gen_analysis/Introgression_Data")
 
-ALL.introgression=read.table("Introgression_Data/fdM_CDS.txt",header=F,stringsAsFactors = T)
-ETS.introgression=read.table("Introgression_Data/ETS_fdM.txt",header=F,stringsAsFactors = T)
-MRP.introgression=read.table("Introgression_Data/ribosomal_fdM.txt",header=F,stringsAsFactors = T)
-MTS.introgression=read.table("Introgression_Data/synthetase_fdM.txt",header=F,stringsAsFactors = T)
-MISC.introgression=read.table("Introgression_Data/miscellaneous_fdM.txt",header=F,stringsAsFactors = T)
+# Define tables from introgression data files
+ALL.introgression=read.table("fdM_CDS.txt",header=T,stringsAsFactors = T)
+ETS.introgression=read.table("ETS_fdM.txt",header=T,stringsAsFactors = T)
+MRP.introgression=read.table("ribosomal_fdM.txt",header=T,stringsAsFactors = T)
+MTS.introgression=read.table("synthetase_fdM.txt",header=T,stringsAsFactors = T)
+MISC.introgression=read.table("miscellaneous_fdM.txt",header=T,stringsAsFactors = T)
 
-colnames(ALL.introgression)<-c("ALL.fdm","ALL.DNAType")
-colnames(ETS.introgression)<-c("ETS.fdm","Gene","ETS.DNAType")
-colnames(MRP.introgression)<-c("MRP.fdm","Gene","MRP.DNAType")
-colnames(MTS.introgression)<-c("MTS.fdm","Gene","MTS.DNAType")
-colnames(MISC.introgression)<-c("MISC.fdm","Gene","MISC.DNAType")
-
+# Define variables containing means of data tables
 mod_ALL.fdm <- ddply(ALL.introgression, "ALL.DNAType", summarise, grp.mean=mean(ALL.fdm))
 mod_ETS.fdm <- ddply(ETS.introgression, "ETS.DNAType", summarise, grp.mean=mean(ETS.fdm))
 mod_MRP.fdm <- ddply(MRP.introgression, "MRP.DNAType", summarise, grp.mean=mean(MRP.fdm))
 mod_MTS.fdm <- ddply(MTS.introgression, "MTS.DNAType", summarise, grp.mean=mean(MTS.fdm))
 mod_MISC.fdm <- ddply(MISC.introgression, "MISC.DNAType", summarise, grp.mean=mean(MISC.fdm))
 
+# Plot all data with titles, x and y labels, coloring lines in density plots
 ALL_fdm_plot <- ggplot(ALL.introgression, aes(x=ALL.fdm, color=ALL.DNAType)) +
-  geom_density() + labs(x="fdM", y = "Density") + theme(legend.position = "none") +
+  geom_text(x=0.15, y=2.2, label="N = 100, P = 0.35", size=10, show.legend = FALSE, colour="black") +
+  geom_density() + labs(title="All Functional Categories", x="fdM", y = "Density") + theme(legend.position = "none") +
   theme(legend.title=element_text(size=50)) + theme(legend.text=element_text(size=45)) +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
+  scale_color_manual(name = "Gene Type", values=c("#af8dc3","#7fbf7b")) + 
   geom_vline(data=mod_ALL.fdm, aes(xintercept=grp.mean, color=ALL.DNAType), linetype="dashed") + 
   theme(text = element_text(size=29))
 ETS_fdm_plot <- ggplot(ETS.introgression, aes(x=ETS.fdm, color=ETS.DNAType)) +
-  geom_density() + labs(x="fdM", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
+  geom_density() + labs(title="Electron Transport System", x="fdM", y = "Density") + theme(legend.position = "none") +
+  scale_color_manual(values=c("#af8dc3","#7fbf7b")) + 
   geom_vline(data=mod_ETS.fdm, aes(xintercept=grp.mean, color=ETS.DNAType), linetype="dashed") +
-  theme(text = element_text(size=19))
+  theme(text = element_text(size=19)) + 
+  geom_text(x=-0.5, y=0.0, label="N = 48, P = 0.382", size=5, colour="black")
 MRP_fdm_plot <- ggplot(MRP.introgression, aes(x=MRP.fdm, color=MRP.DNAType)) +
-  geom_density() + labs(x="fdM", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
+  geom_density() + labs(title="Mito Ribosomal Protein", x="fdM", y = "Density") + theme(legend.position = "none") +
+  scale_color_manual(values=c("#af8dc3","#7fbf7b")) + 
   geom_vline(data=mod_MRP.fdm, aes(xintercept=grp.mean, color=MRP.DNAType), linetype="dashed") +
-  theme(text = element_text(size=19))
+  theme(text = element_text(size=19)) +
+  geom_text(x=0.15, y=2.2, label="N = 29, P = 0.354", size=5, colour="black")
 MTS_fdm_plot <- ggplot(MTS.introgression, aes(x=MTS.fdm, color=MTS.DNAType)) +
-  geom_density() + labs(x="fdM", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
+  geom_density() + labs(title="Mito tRNA-synthetase", x="fdM", y = "Density") + theme(legend.position = "none") +
+  scale_color_manual(values=c("#af8dc3","#7fbf7b")) + 
   geom_vline(data=mod_MTS.fdm, aes(xintercept=grp.mean, color=MTS.DNAType), linetype="dashed") + 
-  theme(text = element_text(size=19))
+  theme(text = element_text(size=19)) +
+  geom_text(x=0.025, y=3.27, label="N = 10, P = 0.685", size=5, colour="black")
 MISC_fdm_plot <- ggplot(MISC.introgression, aes(x=MISC.fdm, color=MISC.DNAType)) +
-  geom_density() + labs(x="fdM", y = "Density") + theme(legend.position = "none") +
-  scale_color_manual(name = "Gene Type", labels = c("N-mt", "Nuclear"), values=c("darkorchid3","dodgerblue1")) + 
+  geom_density() + labs(title="Transcription/translation", x="fdM", y = "Density") + theme(legend.position = "none") +
+  scale_color_manual(values=c("#af8dc3","#7fbf7b")) + 
   geom_vline(data=mod_MISC.fdm, aes(xintercept=grp.mean, color=MISC.DNAType), linetype="dashed") + 
-  theme(text = element_text(size=19))
+  theme(text = element_text(size=19)) +
+  geom_text(x=-0.5, y=0.0, label="N = 13, P = 0.35", size=5, colour="black")
 
+# Create legend using info from ALL_fdm_plot above
 ALL_fdm_legend <- get_legend(ALL_fdm_plot, position = "bottom")
 
+# Combine all plots defined above and add labels
 ggarrange(ALL_fdm_plot,
           ggarrange(ETS_fdm_plot,MRP_fdm_plot,MTS_fdm_plot,MISC_fdm_plot,
-                    labels = c("B", "C", "D","E"), label.x = 0.13, font.label = list(size = 27),
-                    ncol = 2, nrow = 2), labels="A", label.x = 0.13, font.label = list(size = 37), 
+                    labels = c("B", "C", "D","E"), label.x = 0.13, label.y = 0.91, font.label = list(size = 27),
+                    ncol = 2, nrow = 2), labels="A", label.x = 0.13, label.y = 0.93, font.label = list(size = 37), 
           legend.grob = ALL_fdm_legend)
 
-#sink("miscellaneous_fdM_wilcoxon.txt")
+#Change variables according to input file and conduct wilcoxon rank-sum test
 wilcox.test(ALL.fdm ~ ALL.DNAType, data=ALL.introgression, alternative="greater")
-#sink()
